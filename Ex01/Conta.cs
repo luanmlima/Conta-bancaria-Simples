3778f1c934f70d3248cd7f118cd7b66e657615be
Luan Martins <luanm.lima366@gmail.com>
2020-04-08 13:16:50 -0300

    Primeiro Projeto de Conta Bancária Em orientação a objetos

---
+++ Ex01/Conta.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Ex01
+{
+    class Conta
+    {
+        private string _titular;
+        public int nConta { get; private set; }
+        public double vDeposito { get; private set; }
+
+        public Conta(string titular, int conta)
+        {
+            this._titular = titular;
+            this.nConta = conta;
+        }
+
+        public Conta(string titular, int conta, double Deposito) : this(titular, conta)
+        {
+            adicionarValor(Deposito);
+        }
+
+        public string Nome
+        {
+            get { return _titular; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _titular = Nome;
+                }
+
+            }
+        }
+
+        public void adicionarValor(double deposito)
+        {
+            vDeposito += deposito;
+        }
+
+        public void fazerSaque(double saque)
+        {
+            vDeposito -= (saque + 5.00);
+        }
+
+        public override string ToString()
+        {
+            return "Dados da conta:\n" +
+                "Conta " + nConta +
+                ", Titular: " + _titular +
+                ", Saldo: $ " + vDeposito.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Ex01/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char escolha;
+            string nome;
+            int conta;
+            double deposito;
+            double quantia;
+
+            Conta dados;
+
+            Console.Write("Entre com o número da conta: ");
+            conta = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o nome do titular: ");
+            nome = Console.ReadLine();
+            while (nome.Length < 3)
+            {
+                Console.Write("Por favor insira o nome corretamente");
+                Console.Write("\nEntre com o nome do titular: ");
+                nome = Console.ReadLine();
+            };
+
+            Console.Write("Haverá depósito inicial (s/n)?: ");
+            escolha = char.Parse(Console.ReadLine());
+            while (escolha != 's' && escolha != 'n')
+            {
+                Console.Write("Por favor selecione a opção correta.");
+                Console.Write("Deseja Fazer depósito inicial (s/n)?: ");
+                escolha = char.Parse(Console.ReadLine());
+            };
+
+            if (escolha == 's')
+            {
+
+                Console.Write("Entre com o valor para depósito: ");
+                deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                dados = new Conta(nome, conta, deposito);
+
+            }
+            else
+            {
+                dados = new Conta(nome, conta);
+                Console.WriteLine("\n" + dados);
+                Console.WriteLine("\n");
+
+            }
+            Console.WriteLine("\n" + dados);
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.Write("Entre com um valor para depósito: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            dados.adicionarValor(quantia);
+            Console.WriteLine("\n" + dados);
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.Write("Entre com um valor para saque: ");
+            quantia = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            dados.fazerSaque(quantia);
+            Console.WriteLine("\n" + dados);
+            Console.ReadKey();
+
+        }
+    }
+}
